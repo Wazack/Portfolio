cbf57a0ee5a38c8ae728e53e23f93e17179f856f
Wazack <64Anex@gmail.com>
2022-12-07 12:48:47 +0100

    first commit

---
+++ front/src/Intro/Intro.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+
+function Intro() {
+	return (
+		<section>
+			<Navbar />
+		</section>
+	)
+}
+
+export default Intro;
---
+++ front/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Logo from '../assets/logo_ts.png';
+import './Navbar.scss';
+
+function Navbar() {
+
+const [showLinks, setShowLinks] = useState(false);
+
+const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+}
+
+    return (
+        <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <img src={Logo} alt='logo' />
+            <ul>
+                <li><a href='/'>A Propos</a></li>
+                <li><a href='/'>Projets</a></li>
+                <li><a href='/'>Contact</a></li>
+            </ul>
+            <button onClick={handleShowLinks} className='burger'>
+                <span className='burger-bar'></span>
+            </button>
+        </nav>
+    )
+}
+
+export default Navbar;
