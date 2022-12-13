41ef659e58a5f1298f97f4143377e35951c331f9
Wazack <64Anex@gmail.com>
2022-12-13 19:19:54 +0100

    project page in progress

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import About from './About/About';
 import Navbar from './components/Navbar/Navbar';
 import Intro from './Intro/Intro';
+import Projects from './Projects/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Intro />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ front/src/Projects/Projects.tsx
@@ -0,0 +1,20 @@
+import weatherApp from '../components/assets/projects/DisplayWeather.png';
+import transcendence from '../components/assets/projects/transcendence_home.webp';
+import memoji from '../components/assets/memoji/memoji_me_gusta.png';
+import './Projects.scss';
+
+function Projects() {
+    return (
+        <section className="projects">
+            <div className="projects-presentation">
+                <h1>Projets Recents</h1>
+                <div className="project-img">
+                    <img src={weatherApp} alt='Application météo' />
+                    <img src={transcendence} alt='Transcendence' />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Projects
