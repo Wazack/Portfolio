25f89e7034b19f6c087c6f20ef65c45233203e12
Wazack <64Anex@gmail.com>
2022-12-17 16:02:37 +0100

    fix: Projects Page

--- front/src/Projects/Projects.tsx
+++ front/src/Projects/Projects.tsx
@@ -1,7 +1,10 @@
-import weatherApp from '../components/assets/projects/DisplayWeather.png';
-import transcendence from '../components/assets/projects/transcendence_home.webp';
-import memoji from '../components/assets/memoji/memoji_me_gusta.png';
+import weatherApp from '../components/assets/projects/WeatherApp/DisplayWeather.png';
+import weatherAppSearch from '../components/assets/projects/WeatherApp/SearchCity.png';
+import transcendenceHome from '../components/assets/projects/Transcendence/transcendence_home.webp';
+import transcendenceProfile from '../components/assets/projects/Transcendence/ProfileTranscendence.png';
+import transcendenceMap from '../components/assets/projects/Transcendence/SelectMapTranscendence.png';
 import './Projects.scss';
+import Tilt from 'react-parallax-tilt';
 import useNav from '../hooks/useNav';
 
 function Projects() {
@@ -11,8 +14,25 @@ function Projects() {
         <section ref={projectsRef} className="projects" id='projects'>
             <h1>Projets Recents</h1>
             <div className="project-img">
-                <img src={weatherApp} alt='Application météo' />
-                <img src={transcendence} alt='Transcendence' />
+                <div className="weather-app">
+                    <Tilt>
+                        <img src={weatherApp} alt='Application météo' />
+                    </Tilt>
+                    <Tilt>
+                        <img src={weatherAppSearch} alt='Application météo' />
+                    </Tilt>
+                </div>
+                <div className="transcendence">
+                    <Tilt>
+                        <img src={transcendenceHome} alt='Transcendence' />
+                    </Tilt>
+                    <Tilt>
+                        <img src={transcendenceProfile} alt='Profile Transcendence' />
+                    </Tilt>
+                    <Tilt>
+                        <img src={transcendenceMap} alt='Choix map Transcendence' />
+                    </Tilt>
+                </div>
             </div>
         </section>
     )
